73acd3b9c77c854127132eb21d83f1df5a5be4f1
Eric Carter <eric.carter@us.imshealth.com>
2019-05-14 15:39:42 -0700

    Some runner business

--- AdventOfCode.Runner/Program.cs
+++ AdventOfCode.Runner/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AdventOfCode.Runner
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+        public static void ConfigureAppConfiguration(IConfigurationBuilder builder) =>
+            builder.AddCommandLine(Environment.GetCommandLineArgs());
+
+        public static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
         {
-            Console.WriteLine("Hello World!");
+            services.AddHostedService<RunnerService>();
         }
+
+        static Task Main(string[] args)
+        {
+            return new HostBuilder()
+                   //.ConfigureHostConfiguration(ConfigureHostConfiguration)
+                   .ConfigureAppConfiguration(ConfigureAppConfiguration)
+                   .ConfigureServices(ConfigureServices)
+                   //.ConfigureContainer<Container>(ConfigureContainer)
+                   .UseConsoleLifetime()
+                   .RunConsoleAsync(cancellationTokenSource.Token);
+        }
+    }
+
+    public class RunnerService : IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            Program.cancellationTokenSource.Cancel();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
     }
 }
